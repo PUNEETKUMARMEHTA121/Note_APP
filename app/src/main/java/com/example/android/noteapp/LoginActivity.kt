3f40dcdf9762bdb7f4ea47df742234fca08ef2cf
PUNEETKUMARMEHTA121 <p.mehta@flick2know.com>
2021-11-24 11:09:16 +0530

    NoteApp-

--- app/src/main/java/com/example/android/noteapp/LoginActivity.kt
+++ app/src/main/java/com/example/android/noteapp/LoginActivity.kt
@@ -1,11 +1,13 @@
 package com.example.android.noteapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.auth.AuthResult
@@ -13,25 +15,26 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
 class LoginActivity : AppCompatActivity() {
-    lateinit var tv_register:EditText
+     lateinit var tv_register:TextView
     lateinit var btn_login: Button
     lateinit var et_login_email:EditText
     lateinit var et_login_password:EditText
 
-    override fun onCreate(savedInstanceState: Bundle?) {
 
-        tv_register=findViewById<EditText>(R.id.tv_Register)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        tv_register=findViewById(R.id.tv_Register)
         btn_login=findViewById(R.id.btn_login)
         et_login_email=findViewById(R.id.et_email)
         et_login_password=findViewById(R.id.et_password)
 
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
 
         tv_register.setOnClickListener {
             intent.flags=Intent.FLAG_ACTIVITY_NEW_TASK
             startActivity(Intent(this@LoginActivity,RegisterActivity::class.java))
-        finish()
+
         }
 
         btn_login.setOnClickListener {
